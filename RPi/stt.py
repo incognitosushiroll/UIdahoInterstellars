a93939bca4d754c9bb96e253a85a5c4495ebf03d
incognitosushiroll <157520631+incognitosushiroll@users.noreply.github.com>
2025-04-14 10:56:47 -0700

    Update stt.py with the original SOST code for testing purposes

--- RPi/stt.py
+++ RPi/stt.py
@@ -1,40 +1,29 @@
 import argparse
 import stt_selector as stt
-import serial
-import stt_pb2
 
 parser = argparse.ArgumentParser(prog="STT", description="STT main script")
-parser.add_argument("port", type=str, help="Serial Port for RPi")
-parser.add_argument("-b", "--baudrate", type=int, help="Baud rate", default=9600)
+parser.add_argument("type", type=str,
+                    help="The way of running the software: direct_rpi, sample_rpi, sample_stereo.")
+parser.add_argument("catalog", type=int,
+                    help="The degree of catalog separation in the algorithm: 5, 10, 15.")
+parser.add_argument("-exp", "--exptime", type=int,
+                    help="Exposure time (in ms) of the grabbed picture (in direct mode).", default=800)
+parser.add_argument("-n", "--npic", type=int,
+                    help="The number of the picture to analyze (in sample mode).", default=1)
 args = parser.parse_args()
+stt_type = args.type
+cat_division = args.catalog
+n_pic = args.npic
+exp_time = args.exptime
 
-ser = serial.Serial(args.port, args.baudrate)
+if cat_division not in (5, 10, 15):
+    parser.error("Please introduce a valid catalog division: 5/10/15")
 
-while True:
-    buf = ser.read()
-    buf += ser.read(ser.in_waiting)
-    print(buf)
-
-    cmd = stt_pb2.StarTrackerCommand()
-    cmd.ParseFromString(buf)
-
-    if cmd.cat_division not in (5, 10, 15):
-        print("Please introduce a valid catalog division: 5/10/15")
-
-    if cmd.stt_type == "direct_rpi":
-        ra, dec, roll, match_std_x, match_std_y = stt.solve_lis_grab_img(cmd.cat_division, cmd.exp_time)
-    elif cmd.stt_type == "sample_rpi":
-        ra, dec, roll, match_std_x, match_std_y = stt.solve_lis_sample_rpi(cmd.cat_division, cmd.n_pic)
-    elif cmd.stt_type == "sample_stereo":
-        ra, dec, roll, match_std_x, match_std_y = stt.solve_lis_sample_stereo(cmd.cat_division, cmd.n_pic)
-    else:
-        print("---> ERROR: Please introduce a valid option to use this STT software (see the Documentation)!")
-    
-    out = stt_pb2.StarTrackerOutput()
-    out.ra = float(ra)
-    out.dec = float(dec)
-    out.roll = float(roll)
-    out.match_std_x = float(match_std_x)
-    out.match_std_y = float(match_std_y)
-
-    ser.write(out.SerializeToString())
+if stt_type == "direct_rpi":
+    stt.solve_lis_grab_img(cat_division, exp_time)
+elif stt_type == "sample_rpi":
+    stt.solve_lis_sample_rpi(cat_division, n_pic)
+elif stt_type == "sample_stereo":
+    stt.solve_lis_sample_stereo(cat_division, n_pic)
+else:
+    raise NameError("---> ERROR: Please introduce a valid option to use this STT software (see the Documentation)!")
