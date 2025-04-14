ddcedb69cf418e0479d69449c29266cc0fd08688
incognitosushiroll <157520631+incognitosushiroll@users.noreply.github.com>
2025-04-14 13:25:07 -0700

    Update stt_selector.py

--- RPi/stt_selector.py
+++ RPi/stt_selector.py
@@ -1,30 +1,35 @@
 import os
 import subprocess as sp
-import stt_functions as stt
-
-file_dir = os.path.abspath(os.path.dirname(__file__))
-stt_dir = "{}/stt_data".format(file_dir)
 
 def get_photo_index():
-    existing_files = [f for f in os.listdir(stt_dir) if f.startswith("stt_img") and f.endswith(".jpg")] 
-    indices = [int(f.split("_")[-1].split(".")[0]) for f in existing_files if f.split("_")[-1].split(".")[0].isdigit()] 
-    return max(indices, default=0) + 1
+    existing_files = [f for f in os.listdir(stt_dir) if f.startswith("stt_img") and f.endswith(".jpg")]
+    indices = []
 
+    for f in existing_files:
+        try:
+            idx = int(f.split("_")[-1].split(".")[0])
+            indices.append(idx)
+        except ValueError:
+            continue
+
+    return max(indices) + 1 if indices else 0
 
 def solve_lis_grab_img(cat_division, exposure_time_ms):
     exptime_micros = 1000 * exposure_time_ms
     photo_index = get_photo_index()
-    image_dir = os.path.join(stt_dir, f"stt_img{photo_index}.jpg")
-    #image_dir = "{}/stt_img.jpg".format(stt_dir) 
-    print("\n---> STT: Taking picture with {} ms of exposure time.\n".format(exposure_time_ms))
-    task = "libcamera-still -o {} -t 1 --width 1024 --height 1024 --shutter {}".format(image_dir, exptime_micros)
+    image_path = os.path.join(stt_dir, f"stt_img{photo_index}.jpg")
+
+    print(f"\n---> STT: Taking picture with {exposure_time_ms} ms of exposure time.\n")
+
+    task = f"libcamera-still -o {image_path} -t 1 --width 1024 --height 1024 --shutter {exptime_micros}"
     process = sp.Popen(task, shell=True, stdout=sp.PIPE)
     process.wait()
-    return_code = process.returncode
-    #inc_photo += inc_photo 
-    if return_code != 0:
+
+    if process.returncode != 0:
         raise OSError("---> ERROR: The script in the shell was not correctly executed!")
-    return stt.solve_lis(image_dir, cat_division, stt_dir)
+
+    return stt.solve_lis(image_path, cat_division, stt_dir)
+
 
 
 def solve_lis_sample_rpi(cat_division, n_pic):
